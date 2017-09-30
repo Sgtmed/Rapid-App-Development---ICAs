e377035268cd049a2699bde228b217f01c888cde
danbrost <danbrost@gmail.com>
2017-09-30 13:35:26 -0400

    Add files via upload

---
+++ ICA1_DanBrost/ICA1_DanBrost/ICA1_DanBrost.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace ICA1_DanBrost
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnBooks = new System.Windows.Forms.Button();
+            this.btnMusic = new System.Windows.Forms.Button();
+            this.btnPeriodicals = new System.Windows.Forms.Button();
+            this.btnCoffeeBar = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPromotion = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPromotionCode = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.printForm1 = new Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(this.components);
+            this.SuspendLayout();
+            // 
+            // btnBooks
+            // 
+            this.btnBooks.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBooks.Location = new System.Drawing.Point(94, 116);
+            this.btnBooks.Name = "btnBooks";
+            this.btnBooks.Size = new System.Drawing.Size(112, 37);
+            this.btnBooks.TabIndex = 0;
+            this.btnBooks.Text = "&Books";
+            this.btnBooks.UseVisualStyleBackColor = true;
+            this.btnBooks.Click += new System.EventHandler(this.btnBooks_Click);
+            // 
+            // btnMusic
+            // 
+            this.btnMusic.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMusic.Location = new System.Drawing.Point(94, 169);
+            this.btnMusic.Name = "btnMusic";
+            this.btnMusic.Size = new System.Drawing.Size(112, 37);
+            this.btnMusic.TabIndex = 1;
+            this.btnMusic.Text = "&Music";
+            this.btnMusic.UseVisualStyleBackColor = true;
+            this.btnMusic.Click += new System.EventHandler(this.btnMusic_Click);
+            // 
+            // btnPeriodicals
+            // 
+            this.btnPeriodicals.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPeriodicals.Location = new System.Drawing.Point(94, 226);
+            this.btnPeriodicals.Name = "btnPeriodicals";
+            this.btnPeriodicals.Size = new System.Drawing.Size(112, 37);
+            this.btnPeriodicals.TabIndex = 2;
+            this.btnPeriodicals.Text = "P&eriodicals";
+            this.btnPeriodicals.UseVisualStyleBackColor = true;
+            this.btnPeriodicals.Click += new System.EventHandler(this.btnPeriodicals_Click);
+            // 
+            // btnCoffeeBar
+            // 
+            this.btnCoffeeBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCoffeeBar.Location = new System.Drawing.Point(94, 280);
+            this.btnCoffeeBar.Name = "btnCoffeeBar";
+            this.btnCoffeeBar.Size = new System.Drawing.Size(112, 37);
+            this.btnCoffeeBar.TabIndex = 3;
+            this.btnCoffeeBar.Text = "&Coffee Bar";
+            this.btnCoffeeBar.UseVisualStyleBackColor = true;
+            this.btnCoffeeBar.Click += new System.EventHandler(this.btnCoffeeBar_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrint.Location = new System.Drawing.Point(374, 393);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(122, 39);
+            this.btnPrint.TabIndex = 5;
+            this.btnPrint.Text = "&Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(94, 393);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(122, 39);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(629, 393);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(122, 39);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(339, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Promotion Information";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(371, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Current Promotion";
+            // 
+            // lblPromotion
+            // 
+            this.lblPromotion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPromotion.Location = new System.Drawing.Point(547, 116);
+            this.lblPromotion.Name = "lblPromotion";
+            this.lblPromotion.Size = new System.Drawing.Size(388, 52);
+            this.lblPromotion.TabIndex = 9;
+            this.lblPromotion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(371, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Promotion Code";
+            // 
+            // lblPromotionCode
+            // 
+            this.lblPromotionCode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPromotionCode.Location = new System.Drawing.Point(547, 202);
+            this.lblPromotionCode.Name = "lblPromotionCode";
+            this.lblPromotionCode.Size = new System.Drawing.Size(201, 52);
+            this.lblPromotionCode.TabIndex = 11;
+            this.lblPromotionCode.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(917, 530);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Dan Brost";
+            // 
+            // printForm1
+            // 
+            this.printForm1.DocumentName = "document";
+            this.printForm1.Form = this;
+            this.printForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPreview;
+            this.printForm1.PrinterSettings = ((System.Drawing.Printing.PrinterSettings)(resources.GetObject("printForm1.PrinterSettings")));
+            this.printForm1.PrintFileName = null;
+            // 
+            // frmMain
+            // 
+            this.AcceptButton = this.btnClear;
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(978, 552);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblPromotionCode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblPromotion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnCoffeeBar);
+            this.Controls.Add(this.btnPeriodicals);
+            this.Controls.Add(this.btnMusic);
+            this.Controls.Add(this.btnBooks);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "R ‘n R -- For Reading and Refreshment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBooks;
+        private System.Windows.Forms.Button btnMusic;
+        private System.Windows.Forms.Button btnPeriodicals;
+        private System.Windows.Forms.Button btnCoffeeBar;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPromotion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPromotionCode;
+        private System.Windows.Forms.Label label4;
+        private Microsoft.VisualBasic.PowerPacks.Printing.PrintForm printForm1;
+    }
+}
+
---
+++ ICA1_DanBrost/ICA1_DanBrost/ICA1_DanBrost.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Name: Dan Brost
+ * Date: September 22, 2017
+ * Purpose: This application displays a Message that shows the user what promotions are available and the code to us
+ * The output can be cleared. The form can be printed and the user can exit the form.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICA1_DanBrost
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// When user selects it will display a promotion message and code
+        /// </summary>
+        /// <param name="sender">The Books button</param>
+        /// <param name="e">The event arguments</param>
+        private void btnBooks_Click(object sender, EventArgs e)
+        {
+            lblPromotion.Text = "Buy two. Get the second one for half price";
+            lblPromotionCode.Text = "code B608";
+        }
+        /// <summary>
+        /// When user selects it will display a promotion message and code
+        /// </summary>
+        /// <param name="sender">The Music button</param>
+        /// <param name="e">The event arguments</param>
+        private void btnMusic_Click(object sender, EventArgs e)
+        {
+            lblPromotion.Text = "Get a free MP3 download with purchase of a CD";
+            lblPromotionCode.Text = "code M608";
+        }
+        /// <summary>
+        /// When user selects it will display a promotion message and code
+        /// </summary>
+        /// <param name="sender">The Periodicals button</param>
+        /// <param name="e">The event arguments</param>
+        private void btnPeriodicals_Click(object sender, EventArgs e)
+        {
+            lblPromotion.Text = "Elite members receive 10% off every purchase";
+            lblPromotionCode.Text = "code P608";
+        }
+        /// <summary>
+        /// When user selects it will display a promotion message and code
+        /// </summary>
+        /// <param name="sender">The Coffee Bar button</param>
+        /// <param name="e">The event arguments</param>
+        private void btnCoffeeBar_Click(object sender, EventArgs e)
+        {
+            lblPromotion.Text = "Celebrate the season with 20% off specialty drinks";
+            lblPromotionCode.Text = "code C608";
+        }
+        /// <summary>
+        /// When user selects the promotion and code will be cleared
+        /// </summary>
+        /// <param name="sender">The Books button</param>
+        /// <param name="e">The event arguments</param>
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblPromotion.Text = String.Empty;
+            lblPromotionCode.Text = String.Empty;
+        }
+        /// <summary>
+        /// When user selects it will Print Preview
+        /// </summary>
+        /// <param name="sender">The Print button</param>
+        /// <param name="e">The event arguments</param>
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+         printForm1.Print();
+        }
+        /// <summary>
+        /// When user selects it exit the application
+        /// </summary>
+        /// <param name="sender">The Exit button</param>
+        /// <param name="e">The event arguments</param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+         Application.Exit();
+        }
+    }
+}
